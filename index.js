2122ce03ef4d9ea5e476d885695255cfd7d2c9a4
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-01-31 16:13:13 +0700

    Drawl YouTube!

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ CFonts.say('TERMUX WHATSAPP BOT', {
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say('\'wabot-aq\' By @Nurutomo', {
+CFonts.say('\'Youtube:\' Drawl Nag', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
