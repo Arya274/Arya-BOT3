d4afbee5a2bd4f7eb7532a8661f5752f56fe6196
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-02-01 19:03:21 +0700

    Cara Donasi yang benar @arpunnchs

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -7,6 +7,15 @@ let handler = async m => m.reply(`
 ╭─「 Hubungi 」
 │ > Ingin donasi? Wa.me/6281357302007
 ╰────
+
+Ini *#caranya untuk Donasi*
+*Cara Donasi*:
+1.) Beli ke pulsa/ konter terdekat semisal Indomaret
+2.) Bilang ke konter terdekat..
+"Beli pulsa telkomsel"
+3.)Dan terus masukkan nomor kami 081357302007
+4.) Jika sudah kirim bukti ke sini.. Terimakasih
+*Kalau tidak juga gak papa*👍🏻
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
