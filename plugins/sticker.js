f2f9a11e5ab0d32438a3803f6cbc9e87c0492a76
Drawl Nag <75187284+Arya274@users.noreply.github.com>
2021-02-17 07:27:59 +0700

    Add files via upload

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,26 +1,28 @@
 const fetch = require('node-fetch')
-const fs = require('fs')
-const path = require('path')
-const { spawn } = require('child_process')
 const FormData = require('form-data')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, args }) => {
-  let stiker = false
-  try {
-    let q = m.quoted ? m.quoted : m
-    if (/image/.test(q.mimetype || '')) {
-      let img = await q.download()
-      if (!img) throw img
-      stiker = await sticker2(img)
-    } else if (args[0]) stiker = await sticker2(false, args[0])
-  } finally {
-    if (stiker) conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+  let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted }} : m
+  if (/image/.test((m.quoted ? m.quoted : m).mtype)) {
+    let img = await conn.downloadM(q)
+    if (!img) throw img
+    let stiker = await sticker(img)
+stiker = await nStiker(stiker, {
+ author: '@arpunchs',
+ name: 'Nfq BOT'
+})
+    conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+  } else if (args[0]) {
+    let stiker = await sticker(false, args[0])
+    conn.sendMessage(m.chat, stiker, MessageType.sticker, {
       quoted: m
     })
   }
 }
-handler.help = ['stiker (caption|reply media)', 'stiker <url>']
+handler.help = ['stiker']
 handler.tags = ['sticker']
 handler.command = /^stic?ker$/i
 handler.owner = false
@@ -33,44 +35,9 @@ handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
+handler.limit = true
 
 module.exports = handler
-
-let tmp = path.join(__dirname, '../tmp')
-function sticker2(img, url) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      if (url) {
-        let res = await fetch(url)
-        if (res.status !== 200) throw await res.text()
-        img = await res.buffer()
-      }
-      let inp = path.join(tmp, +new Date + '.jpeg')
-      let png = path.join(tmp, +new Date + '.png')
-      let out = path.join(tmp, +new Date + '.webp')
-      fs.writeFileSync(inp, img)
-      spawn('ffmpeg', [
-        '-y',
-        '-i', inp,
-        '-vf', 'scale=512:512:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=512:512:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1',
-        png
-      ])
-      .on('error', reject)
-      .on('close', () => {
-        fs.unlinkSync(inp)
-        spawn('convert', [png, out])
-        .on('error', reject)
-        .on('close', () => {
-          fs.unlinkSync(png)
-          resolve(fs.readFileSync(out))
-          fs.unlinkSync(out)
-        })
-      })
-    } catch (e) {
-      reject(e)
-    }
-  })
-}
 
 async function canvas(code, type = 'png', quality = 0.92) {
     let res = await fetch('https://nurutomo.herokuapp.com/api/canvas?' + queryURL({
@@ -127,4 +94,29 @@ function uploadImage(buffer) {
             reject(e)
         }
     })
+}
+let { spawnSync } = require('child_process')
+let fs = require('fs')
+async function nStiker(input, options) {
+    const exifPath = 'data.exif'
+    const resultPath = 'sticker.webp'
+    fs.writeFileSync(resultPath, input)
+    // Custom?
+    const stickerpackid = "com.snowcorp.stickerly.android.stickercontentprovider b5e7275f-f1de-4137-961f-57becfad34f2" //not sure what this does
+    const packname = options.name || "st4rz BOT";
+    const author = options.author || "@bintang_nur_pradana";
+    const googlelink = "https://play.google.com/store/apps/details?id=com.marsvard.stickermakerforwhatsapp";
+    const applelink = "https://itunes.apple.com/app/sticker-maker-studio/id1443326857";
+
+    const json = { "sticker-pack-id": stickerpackid, "sticker-pack-name": packname, "sticker-pack-publisher": author, "android-app-store-link": googlelink, "ios-app-store-link": applelink }
+    let exifAttr = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0x00, 0x00, 0x00])
+    let jsonBuffer = Buffer.from(JSON.stringify(json), 'utf8')
+    let exif = Buffer.concat([exifAttr, jsonBuffer])
+    exif.writeUIntLE(jsonBuffer.length, 14, 4)
+    fs.writeFileSync(exifPath, exif)
+    spawnSync('webpmux', [
+        '-set', 'exif', exifPath,
+        '-o', resultPath, resultPath
+    ])
+    return fs.readFileSync(resultPath)
 }
