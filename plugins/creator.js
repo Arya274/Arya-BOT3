a2a26d3f197e862f7e250932f51f613dfc86eb8a
dhdhdhdhhejwh <80098534+dhdhdhdhhejwh@users.noreply.github.com>
2021-03-08 10:39:15 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288235435804', 'Admin DrawlNag', m)
+  this.sendContact(m.chat, '6287871537199', 'Admin bot', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
